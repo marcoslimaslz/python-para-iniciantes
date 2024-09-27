6751bef89ea4a9d10cc34b74290bfea6413b47ab
Marcos Lima <marcosbew2@hotmail.com>
2024-09-27 11:20:20 -0300

    Create print.py

---
+++ print.py
@@ -0,0 +1,8 @@
+email = "usuario@email.com"
+
+print(email)
+print("Email ->", email)
+print(f"Email -> {email}")
+print(f"Email -> \n{email}")
+print(f"Email -> \t{email}")
+print(f"Email -> \n\t{email}")
